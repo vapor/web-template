56d6a17d8cc7d779ad6cdfddb419fb9cf838f665
Jason Kuang <jason@startrails.cn>
2018-07-04 11:08:53 +0800

    change vapor version

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
     ],
     targets: [
